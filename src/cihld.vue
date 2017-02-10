e1ca2ca9a5353d04dfc11215f40f35e2e0ff6e38
wemlion <angusfu1126@qq.com>
2017-02-11 00:25:21 +0800

    basic structure and styles

---
+++ src/cihld.vue
@@ -0,0 +1,44 @@
+<template>
+<ul>
+  <li>{{nodeName}}</li>
+  <li v-for="branch in branches" class="branch">
+    {{branch}}
+  </li> 
+
+  <li v-for="leaf in leaves" class="leaf">
+    {{leaf}}
+  </li>
+</ul>
+</template>
+
+<script>
+  export default {
+    props: {
+      data: Object,
+      conf: Object
+    },
+    computed: {
+      branches() {
+        return this.data[this.conf.branch];
+      },
+      leaves() {
+        return this.data[this.conf.leaf];
+      },
+      nodeName() {
+        return this.data[this.conf.node];
+      }
+    },
+    created() {
+      console.log(this.data);
+    }
+  };
+</script>
+
+<style>
+  .branch {
+    color: red;
+  }
+  .leaf {
+    color: lightseagreen;
+  }
+</style>
--- src/index.vue
+++ src/index.vue
@@ -1,12 +1,81 @@
 <template>
-  <p>Hello world!!!</p>
+<ul class="branch">
+  <li class="node" @click="toggleNode(data)">
+    <i class="fa" :class="{
+      'fa-folder': !nodeExpanded,
+      'fa-folder-open': nodeExpanded
+    }"></i>
+    {{nodeName}}
+  </li> 
+
+  <v-tree-select
+    v-show="nodeExpanded"
+    v-for="branch in branches"
+    :data="branch"
+    :conf="conf"
+  ></v-tree-select> 
+
+
+  <li v-show="nodeExpanded" v-for="leaf in leaves" class="leaf">
+    {{leaf}}
+  </li>
+</ul>
 </template>
 
 <script>
   export default {
     name: 'vTreeSelect',
+    props: {
+      data: Object,
+      conf: Object
+    },
+    data() {
+      return {
+        nodeExpanded: false
+      };
+    },
+    computed: {
+      branches() {
+        return this.data[this.conf.branch];
+      },
+      leaves() {
+        return this.data[this.conf.leaf];
+      },
+      nodeName() {
+        return this.data[this.conf.node];
+      }
+    },
+    methods: {
+      toggleNode(data) {
+        this.nodeExpanded = !this.nodeExpanded;
+      }
+    },
     created() {
-      console.log('created');
+      console.log(this.conf.node);
     }
   };
 </script>
+
+<style>
+  .branch {
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+    color: #666;
+  }
+  .node {
+    cursor: pointer;
+  }
+  .node > .fa {
+    color: #0d83e6;
+  }
+  .leaf {
+    margin-left: 15px;
+    cursor: pointer;
+  }
+
+  .branch > .branch {
+    margin-left: 15px;
+  }
+</style>
