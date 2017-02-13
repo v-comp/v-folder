52b4641cce5eec1ad07bb167abdf0fee03150d2b
wemlion <angusfu1126@qq.com>
2017-02-13 20:29:09 +0800

    remove v-tree

--- src/index.vue
+++ src/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <v-tree :store="store" :uid="uid"></v-tree>
+  <ul class="v-branch-body">
+    <v-node :data="node" :uid="uid"></v-node>
+    <v-branch v-show="node.open" v-for="branch in branches" :data="branch" :uid="uid"></v-branch>
+    <v-leaf v-show="node.open" v-for="leaf in leafs" :data="leaf" :uid="uid"></v-leaf>
+  </ul>
 </template>
 <script>
   let KEY_MAP = {};
-  import EventMixin from './mixin';
   import Store from './store';
-  import VTree from './v-tree.vue';
+  import EventMixin from './mixin';
+  import VNode from './v-node.vue';
+  import VLeaf from './v-leaf.vue';
+  import VBranch from './v-branch.vue';
 
   export default {
     name: 'v-folder',
@@ -20,12 +26,28 @@
       conf: Object
     },
     components: {
-      'v-tree': VTree
+      'v-node': VNode,
+      'v-leaf': VLeaf,
+      'v-branch': VBranch
     },
     data() {
       return {
         store: new Store(this.data, this.conf)
       };
+    },
+    computed: {
+      root() {
+        return this.store.dataStore;
+      },
+      branches() {
+        return this.root.branches;
+      },
+      leafs() {
+        return this.root.leafs;
+      },
+      node() {
+        return this.root.node;
+      }
     },
     methods: {
       request(node, done) {
@@ -89,3 +111,95 @@
     }
   };
 </script>
+
+<style>
+  /*----------------------------------------------------------------
+                            .v-branch
+  ---------------------------------------------------------------*/
+  .v-branch-body {
+    padding: 0;
+    font-size: 18px;
+    color: #666;
+    list-style: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+  }
+  .v-branch-body .v-branch {
+    position: relative;
+    padding-left: 27px;
+  }
+  /*.v-branch:before,
+  .v-branch:after,
+  .v-leaf:before,
+  .v-leaf:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    width: 25px;
+    height: 50%;
+    border-left: 1px solid;
+    border-bottom: 1px solid;
+  }
+  .v-branch:after, 
+  .v-leaf:after {
+    border-bottom: 0;
+    bottom: 0;
+  }*/
+  .v-branch ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  /*----------------------------------------------------------------
+                            .v-node
+  ---------------------------------------------------------------*/
+  .v-node {
+    padding: 0;
+    cursor: pointer;
+    list-style: none;
+  }
+  .v-node .fa {
+    width: 20px;
+    color: #0d83e6;
+    text-align: center;
+  }
+  .v-node .fa:hover {
+    color: #0c71c5;
+  }
+
+  /*----------------------------------------------------------------
+                            .v-leaf
+  ---------------------------------------------------------------*/
+  .v-leaf {
+    position: relative;
+    margin: 0;
+    padding: 0 0 0 27px;
+    cursor: pointer;
+  }
+  .v-leaf .fa {
+    display: inline-block;
+    width: 20px;
+    color: #0d83e6;
+    text-align: center;
+    font: normal normal normal 14px/1 FontAwesome;
+    font-size: inherit;
+    text-rendering: auto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale
+  }
+  .v-leaf .fa:hover {
+    color: #0c71c5;
+  }
+
+  .cursor-no-ops {
+    cursor: not-allowed;
+  }
+  .cursor-progress {
+    cursor: progress;
+  }
+</style>
+
+
